4931c8c4626eb50fc3401563ca0b201eb2a5d0f6
Kostia S <kosiak5587@gmail.com>
2024-11-11 10:37:33 +0600

    feat: add Swagger initialization to main bootstrap process

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { setupSwagger } from '../swagger';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(PORT);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+  console.log(`Swagger is running on: ${await app.getUrl()}/api`);
 }
 bootstrap();
--- swagger.ts
+++ swagger.ts
@@ -4,16 +4,14 @@ import * as fs from 'fs';
 
 export function setupSwagger(app: INestApplication) {
   const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription('API description for my NestJS app')
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
     .setVersion('1.0')
-    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
-  // Генерация файла api.yaml
-  fs.writeFileSync('./api.yaml', JSON.stringify(document, null, 2));
+  fs.writeFileSync('./doc/api.yaml', JSON.stringify(document, null, 2));
 
   SwaggerModule.setup('api', app, document);
 }
